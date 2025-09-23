bae324a3fa4bb3f9a55d41a3f9cc5665e60af8d7
faizan <faizanamjad575@gmail.com>
2025-09-23 18:48:06 +0500

    create script to populate database

---
+++ database/populate.js
@@ -0,0 +1,42 @@
+#! usr/bin/env node
+
+const { Client } = require("pg");
+require("dotenv").config();
+
+const SQL = `
+CREATE TABLE IF NOT EXISTS users(
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(50),
+  username VARCHAR(50),
+  password VARCHAR(200),
+  admin BOOLEAN,
+  member BOOLEAN
+);
+
+CREATE TABLE IF NOT EXISTS posts(
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  title VARCHAR (100),
+  message VARCHAR (250),
+  author_id INTEGER,
+  time TIMESTAMPTZ
+);
+
+INSERT INTO users (name, username, password, admin, member) VALUES ('Akif Emir', 'akif1234', 1234, FALSE, FALSE);
+`;
+
+async function main() {
+  try {
+    console.log("seeding...");
+    const client = new Client({
+      connectionString: process.env.DB,
+    });
+    await client.connect();
+    await client.query(SQL);
+    await client.end();
+    console.log("process completed successfully");
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+main();
