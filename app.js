63271d150eecc5ab3f0d25828768555a6419f818
faizan <faizanamjad575@gmail.com>
2025-09-23 18:57:47 +0500

    fix minor bug

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const router = require("./routes/router");
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
-app.use("view engine", "ejs");
-app.use("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 app.use(router);
 
 app.listen(3000, (error) => {
